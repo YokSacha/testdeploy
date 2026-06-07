31f1e0467a711db8051f872ffca36c06295ec585
poomichopoom-p <Poomicho.p@gmail.com>
2026-06-07 20:28:06 +0700

    addFunctionQuickConfiem

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: "https://kinetix-qnx5.onrender.com",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
--- src/components/admin/QuickConfirm.jsx
+++ src/components/admin/QuickConfirm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import API from "../../api/axios.js";
 
 const MOCK_DRIVERS = [
   { id: "DRV-001", name: "สมชาย ขับดี" },
