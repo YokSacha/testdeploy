cb434a5f48d1e53c454df58a6bc89e77759ee4a8
YokSacha <khumhom.s@gmail.com>
2026-06-11 17:11:55 +0700

    Fixed circular dependency and verified local build

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:5000/"
-//"https://kinetix-qnx5.onrender.com";
+  "https://testdeploy-backend-h8lz.onrender.com"
 
 const API = axios.create({
   baseURL: API_BASE_URL,
